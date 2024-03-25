0c8614f413fafdaae2045dfa433262a07a073ac4
Thatotheron32 <157820420+Thatotheron32@users.noreply.github.com>
2024-03-25 02:31:47 -0500

    Need to fix the HTML and the RC valuing

--- calc.py
+++ calc.py
@@ -16,7 +16,7 @@ def Valueset(V,R,C,T):
 #Real Value Calculation of the function
 def rccalc():
     global voltage, RVal, CVal, timeMax
-    val = [] #List of calcuated Voltage Values.
+    val = [] #List of calculated Voltage Values.
     i = 0
     while i < timeMax: #Starts a loop that stops when it's equal to asked time.
         val.append(voltage*math.e**(-1*((i/100)/(RVal*CVal)))) # Add newly calculated Voltage to the next spot on the list.
@@ -31,13 +31,13 @@ def eulers(step):
     x = [0] # Time steps 
     Yval = [voltage] # Intital Voltage
     Vval = [-1*(Yval[i-1]/(RVal*CVal))] # First DV/DT
-    deltaY = [Vval[i-1] * step] # First step of Delta Y
+    deltaY = [Vval[i-1] * step/100] # First step of Delta Y
     Nval = [0] # how many times it was taken. 
     while i < timeMax: #Starts a loop that stops when it's equal to asked time.
-       x.append(x[i-1] + step) # Increments the time step. 
+       x.append(x[i-1] + step/100) # Increments the time step. 
        Yval.append(Yval[i-1] + deltaY[i-1]) # New Y val calculation. 
        Vval.append(-1*(Yval[i] / (RVal * CVal))) # DV/Dt Calculation. 
-       deltaY.append(Vval[i] * step) # New Delta Y calculation. 
+       deltaY.append(Vval[i] * step/100) # New Delta Y calculation. 
        Nval.append(i) # Step calculation. 
        i += 1
     return Yval,Vval, x,deltaY, Nval
--- rc.py
+++ rc.py
@@ -1,7 +1,19 @@
-from flask import Blueprint, render_template
-
+from tokenize import Double
+from flask import Blueprint, render_template, request
+from calc import Valueset, rccalc, eulers
 rc = Blueprint(__name__, "rc")
 
 @rc.route("/")
 def rccalc():
+    x = 0
+    CalcV = 0
     return render_template("rc.html")
+
+@rc.route("/", methods = ['POST'])
+def getvalue():
+    Valueset(Double(request.form['IVolt']),Double(request.form['ResVal']),Double(request.form['CapVal']),Double(request.form['Maxtime']))
+    CalcV = rccalc()
+    Voltage,dvdx,x,deltaY,steps =  eulers(Double(request.form['Steptime']))
+    return render_template('rc.html')
+    
+
