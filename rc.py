0060192d7bed72cdd96a1d00b79eda9ef821a237
Thatotheron32 <157820420+Thatotheron32@users.noreply.github.com>
2024-03-26 08:20:39 -0500

    Table is now working

--- rc.py
+++ rc.py
@@ -3,12 +3,16 @@ from flask import Blueprint, render_template, request
 from calc import Valueset, rcsolcalc, eulers
 rc = Blueprint(__name__, "rc")
 
+SolCord = []
+EulersCord =[]
+
 @rc.route("/")
 def rccalc():
-    return render_template("rc.html")
+    return render_template("rc.html", SolCord = SolCord, EulersCord = EulersCord)
 
 @rc.route("/", methods = ['POST'])
 def getvalue():
+    global SolCord, EulersCord
     Valueset(request.form['IVolt'], request.form['ResVal'], request.form['CapVal'], request.form['Maxtime'])
     SolCord = rcsolcalc() 
     EulersCord = eulers(request.form['Steptime'])
